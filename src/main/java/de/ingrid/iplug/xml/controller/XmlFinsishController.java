177815f923898f210664985b0feb8ddd2e94cfb1
ak.101tec <ak.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-30 10:05:31 +0000

    o add finish controller

---
+++ src/main/java/de/ingrid/iplug/xml/controller/XmlFinsishController.java
@@ -0,0 +1,47 @@
+package de.ingrid.iplug.xml.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import de.ingrid.admin.IUris;
+import de.ingrid.admin.command.PlugdescriptionCommandObject;
+import de.ingrid.iplug.xml.model.Field;
+
+@Controller
+@RequestMapping("/iplug/finish.html")
+@SessionAttributes(value = { "plugDescription", "document" })
+public class XmlFinsishController {
+	@SuppressWarnings("unchecked")
+	@RequestMapping(method = RequestMethod.POST)
+	public String postFinish(
+			@ModelAttribute("plugDescription") PlugdescriptionCommandObject plugdescriptionCommandObject,
+			@ModelAttribute("document") de.ingrid.iplug.xml.model.Document document)
+			throws IOException {
+
+		if (!plugdescriptionCommandObject.containsKey("fields")) {
+			plugdescriptionCommandObject.put("fields", new ArrayList<String>());
+		}
+
+		List<String> fields = (List<String>) plugdescriptionCommandObject
+				.get("fields");
+
+		plugdescriptionCommandObject.put("document", document);
+
+		List<Field> fieldsFromDocument = document.getFields();
+		for (Field field : fieldsFromDocument) {
+			String fieldName = field.getFieldName();
+			fields.add(fieldName);
+		}
+
+		return "redirect:" + IUris.SAVE;
+	}
+}
