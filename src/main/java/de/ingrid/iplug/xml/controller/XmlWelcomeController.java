81c0134494ad50f338d5216a3b22a27bf3042156
fh <fh@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-11-02 13:23:55 +0000

    fix initial redirect to listMappings

--- src/main/java/de/ingrid/iplug/xml/controller/XmlWelcomeController.java
+++ src/main/java/de/ingrid/iplug/xml/controller/XmlWelcomeController.java
@@ -12,6 +12,6 @@ public class XmlWelcomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String welcome() {
-		return "redirect:/iplug/upload.html";
+		return "redirect:/iplug/listMappings.html";
 	}
 }
