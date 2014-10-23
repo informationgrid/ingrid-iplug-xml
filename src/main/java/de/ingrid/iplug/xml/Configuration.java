21972fcfcae0dd3dac2c9130be0075997b9f66dc
ktt <ktt@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-10-23 09:28:13 +0000

    [IPLUG-XML]:
    Edit save override properties.

--- src/main/java/de/ingrid/iplug/xml/Configuration.java
+++ src/main/java/de/ingrid/iplug/xml/Configuration.java
@@ -1,27 +1,18 @@
 package de.ingrid.iplug.xml;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.thoughtworks.xstream.XStream;
 import com.tngtech.configbuilder.annotation.propertyloaderconfiguration.PropertiesFiles;
 import com.tngtech.configbuilder.annotation.propertyloaderconfiguration.PropertyLocations;
-import com.tngtech.configbuilder.annotation.typetransformer.TypeTransformer;
-import com.tngtech.configbuilder.annotation.typetransformer.TypeTransformers;
 import com.tngtech.configbuilder.annotation.valueextractor.DefaultValue;
 import com.tngtech.configbuilder.annotation.valueextractor.PropertyValue;
 
 import de.ingrid.admin.IConfig;
 import de.ingrid.admin.command.PlugdescriptionCommandObject;
-import de.ingrid.iplug.xml.model.Document;
-import de.ingrid.iplug.xml.model.Field;
-import de.ingrid.iplug.xml.model.FieldType;
-import de.ingrid.iplug.xml.model.Filter;
-import de.ingrid.iplug.xml.model.Filter.FilterType;
 
 @PropertiesFiles( {"config"} )
 @PropertyLocations(directories = {"conf"}, fromClassLoader = true)
@@ -29,10 +20,11 @@ public class Configuration implements IConfig {
     
     private static Log log = LogFactory.getLog(Configuration.class);
     
-    @TypeTransformers(Configuration.StringToCommunications.class)
     @PropertyValue("plugdescription.mapping")
     @DefaultValue("")
-    public List<Document> documents;
+    public String mapping;
+    
+    private XStream xstream;
     
     @Override
     public void initialize() {
@@ -43,94 +35,18 @@ public class Configuration implements IConfig {
         pdObject.addField("metainfo");
         pdObject.put( "iPlugClass", "de.ingrid.iplug.xml.XmlPlug");
         if(pdObject.get("mapping") == null){
-        	if(documents.size() > 0){
-        		pdObject.put("mapping", documents);
+        	if(mapping != null){
+        		xstream = new XStream();
+            	pdObject.put("mapping", xstream.fromXML(mapping));
         	}
         }
     }
 
     @Override
     public void setPropertiesFromPlugdescription( Properties props, PlugdescriptionCommandObject pd ) {
-    	String mappingString = "";
-    	List<Document> documents = (List<Document>) pd.get("mapping");
-    	for (int i = 0; i < documents.size(); i++) {
-			Document document = documents.get(i);
-			mappingString = mappingString + "" + document.getDescription() + ",";
-			mappingString = mappingString + "" + document.getFileName() + ",";
-			mappingString = mappingString + "" + document.getRootXpath();
-			List<Field> fields = document.getFields();
-			for (int j = 0; j < fields.size(); j++) {
-				mappingString = mappingString + "##";
-				
-				Field field = fields.get(j);
-				mappingString = mappingString + "" + field.getFieldName() + ",";
-				mappingString = mappingString + "" + field.getFieldType() + ",";
-				mappingString = mappingString + "" + field.getXpath() + ",";
-				mappingString = mappingString + "" + field.getScore();
-				List<Filter> filters = field.getFilters();
-				for (int k = 0; k < filters.size(); k++) {
-					mappingString = mappingString + ",";
-					Filter filter = filters.get(k);
-					mappingString = mappingString + "" + filter.getFilterType() + ",";
-					mappingString = mappingString + "" + filter.getExpression();
-				}
-			}
-			if(i != documents.size() - 1){
-				mappingString = mappingString + "###";
-			}
-		}
-    	if(mappingString.length() > 0){
-    		props.setProperty("plugdescription.mapping", mappingString);
+    	if(pd.get("mapping") != null){
+    		xstream = new XStream();
+        	props.setProperty("plugdescription.mapping", xstream.toXML(pd.get("mapping")));
     	}
-    }
-    
-    public class StringToCommunications extends TypeTransformer<String, List<Document>> {
-
-        @Override
-        public List<Document> transform(String input) {
-            List<Document> list = new ArrayList<Document>();
-            String[] mappingLists = input.split("###");
-            if (mappingLists.length > 0) {
-            	for (int i = 0; i < mappingLists.length; i++) {
-            		Document document = new Document();
-                    String mappingList = mappingLists[i];
-    				String[] mappingListEntries = mappingList.split("##");
-    				if(mappingListEntries.length > 0){
-    					for (int j = 0; j < mappingListEntries.length; j++) {
-    						String mappingListEntry = mappingListEntries[j];
-    						String[] fields = mappingListEntry.split(",");
-    						if(fields.length > 0){
-    							if(j == 0){
-    								String description = fields[0];
-    								String filename = fields[1];
-    								String rootxpath = fields[2];
-    								
-    								document.setDescription(description);
-    								document.setFileName(filename);
-    								document.setRootXpath(rootxpath);
-        						}else{
-        							String label = fields[0];
-        							String fieldType = fields[1];
-        							String xpath = fields[2];
-        							float score = Float.parseFloat(fields[3]);
-        							
-        							Field field = new Field(label, xpath, score, FieldType.valueOf(fieldType));
-        							if(fields.length > 4){
-        								String filterTypeString = fields[4];
-            							Comparable<? extends Serializable> expression = fields[5];
-            							
-            							Filter filter = new Filter(expression, FilterType.valueOf(filterTypeString));
-            							field.addFilter(filter);
-        							}
-        							document.addField(field);
-        						}
-    						}
-    					}
-    				}
-    				list.add(document);
-    			}
-            }
-            return list;
-        }
     }
 }
