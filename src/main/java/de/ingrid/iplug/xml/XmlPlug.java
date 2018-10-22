3bbb5100540c3e4c8f8cee4a8592646ba673163f
Andre Wallat <andre.wallat@wemove.com>
2018-10-22 11:53:32 +0200

    Implement call method to start indexing from iBus

--- src/main/java/de/ingrid/iplug/xml/XmlPlug.java
+++ src/main/java/de/ingrid/iplug/xml/XmlPlug.java
@@ -22,6 +22,9 @@
  */
 package de.ingrid.iplug.xml;
 
+import de.ingrid.admin.elasticsearch.IndexScheduler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -47,24 +50,28 @@ import de.ingrid.utils.query.IngridQuery;
 @Service
 public class XmlPlug extends HeartBeatPlug implements IRecordLoader {
 
+	private static Logger log = LogManager.getLogger(XmlPlug.class);
+
 	private final IndexImpl _indexSearcher;
 	public static Configuration conf;
-	
+	private final IndexScheduler indexScheduler;
+
 	@Autowired
 	public XmlPlug(final IndexImpl indexSearcher,
 			final IPlugdescriptionFieldFilter[] fieldFilters,
 			final IMetadataInjector[] metadataInjectors,
-			final IPreProcessor[] preProcessors, final IPostProcessor[] postProcessors) {
+			final IPreProcessor[] preProcessors, final IPostProcessor[] postProcessors, IndexScheduler indexScheduler) {
 		super(60000, new PlugDescriptionFieldFilters(fieldFilters),
 				metadataInjectors, preProcessors, postProcessors);
 		_indexSearcher = indexSearcher;
+		this.indexScheduler = indexScheduler;
 	}
 
 	/* (non-Javadoc)
 	 * @see de.ingrid.iplug.HeartBeatPlug#close()
 	 */
 	@Override
-	public void close() throws Exception {
+	public void close() {
 		_indexSearcher.close();
 	}
 
@@ -81,37 +88,42 @@ public class XmlPlug extends HeartBeatPlug implements IRecordLoader {
 	 * @see de.ingrid.utils.IDetailer#getDetail(de.ingrid.utils.IngridHit, de.ingrid.utils.query.IngridQuery, java.lang.String[])
 	 */
 	public IngridHitDetail getDetail(final IngridHit hit,
-			final IngridQuery query, final String[] fields) throws Exception {
-		final IngridHitDetail detail = _indexSearcher.getDetail(hit, query,
-				fields);
-		return detail;
+			final IngridQuery query, final String[] fields) {
+		return _indexSearcher.getDetail(hit, query, fields);
 	}
 
 	/* (non-Javadoc)
 	 * @see de.ingrid.utils.IDetailer#getDetails(de.ingrid.utils.IngridHit[], de.ingrid.utils.query.IngridQuery, java.lang.String[])
 	 */
 	public IngridHitDetail[] getDetails(final IngridHit[] hitArray,
-			final IngridQuery query, final String[] fields) throws Exception {
-		final IngridHitDetail[] details = _indexSearcher.getDetails(hitArray,
-				query, fields);
-		return details;
+			final IngridQuery query, final String[] fields) {
+		return _indexSearcher.getDetails(hitArray, query, fields);
 	}
 
 	/* (non-Javadoc)
 	 * @see de.ingrid.utils.IRecordLoader#getRecord(de.ingrid.utils.IngridHit)
 	 */
 	@Override
-	public Record getRecord(final IngridHit hit) throws Exception {
+	public Record getRecord(final IngridHit hit) {
         return _indexSearcher.getRecord(hit);
 	}
 	
 	public static void main(String[] args) throws Exception {
-        conf = new ConfigBuilder<Configuration>(Configuration.class).withCommandLineArgs(args).build();
+        conf = new ConfigBuilder<>(Configuration.class).withCommandLineArgs(args).build();
         new JettyStarter( conf );
     }
 
-    @Override
-    public IngridDocument call(IngridCall targetInfo) throws Exception {
-        throw new RuntimeException( "call-function not implemented in XML-iPlug" );
-    }
+	@Override
+	public IngridDocument call(IngridCall info) {
+		IngridDocument doc = null;
+
+		if ("index".equals(info.getMethod())) {
+			indexScheduler.triggerManually();
+			doc = new IngridDocument();
+			doc.put("success", true);
+		}
+		log.warn("The following method is not supported: " + info.getMethod());
+
+		return doc;
+	}
 }
